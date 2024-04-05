b355c722f6ea493e3eab233ec12b6fb2e20edffa
itte1 <nakamura@itte.dev>
2024-04-05 14:57:31 +0900

    Add vue examples

---
+++ examples/vue/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+const value = ref(null)
+</script>
+
+<template>
+  <div>
+    value: {{ value }}
+  </div>
+
+  <div>
+    <input-dt v-model="value">
+      <input type="text" input-dt></input>
+    </input-dt>
+  </div>
+</template>
---
+++ examples/vue/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'input-dt'
+import 'input-dt/input-dt.css'
+
+const app = createApp(App)
+app.mount('#app')
