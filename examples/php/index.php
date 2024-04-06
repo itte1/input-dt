eeba039e19f77c8453b372f1c0eae9623570e807
itte1 <nakamura@itte.dev>
2024-04-06 21:21:21 +0900

    Add PHP example

---
+++ examples/php/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/input-dt@0.0.3/input-dt-min.css">
+</head>
+<body>
+  <div>
+    Value: <?=$_POST['calender']?>
+  </div>
+  <div>
+    <form method="post" action=".">
+      <input-dt value="<?=$_POST['calender']?>">
+        <input type="text" input-dt>
+        <input type="hidden" name="calender" input-dt-value="YYYY-MM-DD HH:mm:ss">
+      </input-dt>
+      <button type="submit">送信</button>
+    </form>
+  </div>
+  <script type="module" src="https://cdn.jsdelivr.net/npm/input-dt@0.0.3/input-dt-min.js"></script>
+</body>
+</html>
